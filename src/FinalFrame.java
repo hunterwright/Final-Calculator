a2938ab128fea80a53f55f5ad6e785e1e6aeae95
hunterwright <wright.hunter22@gmail.com>
2017-02-23 14:34:13 -0600

    Started on setup of one textField
    Also added frame setup

--- src/FinalFrame.java
+++ src/FinalFrame.java
@@ -1,6 +1,25 @@
+import javax.swing.JFrame;
+import javax.swing.JTextField;
+import java.awt.Font;
+
 /**
  * Frame of the final calculator
  */
-public class FinalFrame {
-    
+public class FinalFrame extends JFrame{
+    JTextField trmW = new JTextField("");
+
+    public FinalFrame() {
+        // FRAME SETUP -----------------------------------------
+        super("Simple Calculator");
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setSize(450, 600);
+        setLayout(null);
+        setVisible(true);
+        setResizable(false);
+        //------------------------------------------------------
+
+        trmW.setBounds(225,25,200,40);
+        trmW.setFont(new Font("Times New Roman",Font.PLAIN,25));
+        add(trmW);
+    }
 }
